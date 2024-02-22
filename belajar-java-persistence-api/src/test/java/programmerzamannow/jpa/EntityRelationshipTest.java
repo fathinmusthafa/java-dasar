214a5d45de71c46c0fc792af2c46bf09ada8f58e
fathinmusthafa <fathinmusthafa17@gmail.com>
2024-02-22 14:06:19 +0700

    update belajar java web servlet

---
+++ belajar-java-persistence-api/src/test/java/programmerzamannow/jpa/EntityRelationshipTest.java
@@ -0,0 +1,135 @@
+package programmerzamannow.jpa;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.EntityTransaction;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import programmerzamannow.jpa.entity.*;
+import programmerzamannow.jpa.util.JpaUtil;
+
+public class EntityRelationshipTest {
+
+    @Test
+    void oneToOnePersist() {
+        EntityManagerFactory entityManagerFactory = JpaUtil.getEntityManagerFactory();
+        EntityManager entityManager = entityManagerFactory.createEntityManager();
+        EntityTransaction entityTransaction = entityManager.getTransaction();
+        entityTransaction.begin();
+
+        Credential credential = new Credential();
+        credential.setId("habib");
+        credential.setEmail("habib@gmail.com");
+        credential.setPassword("rahasia");
+        entityManager.persist(credential);
+
+        User user = new User();
+        user.setId("habib");
+        user.setName("Fathin Musthafa");
+        entityManager.persist(user);
+
+        entityTransaction.commit();
+        entityManager.close();
+    }
+
+    @Test
+    void oneToOneFind() {
+        EntityManagerFactory entityManagerFactory = JpaUtil.getEntityManagerFactory();
+        EntityManager entityManager = entityManagerFactory.createEntityManager();
+        EntityTransaction entityTransaction = entityManager.getTransaction();
+        entityTransaction.begin();
+
+        User user = entityManager.find(User.class, "habib");
+        Assertions.assertNotNull(user.getCredential());
+
+        Assertions.assertEquals("habib@gmail.com", user.getCredential().getEmail());
+        Assertions.assertEquals("rahasia", user.getCredential().getPassword());
+
+        entityTransaction.commit();
+        entityManager.close();
+    }
+
+    @Test
+    void oneToOneJoinColumn() {
+        EntityManagerFactory entityManagerFactory = JpaUtil.getEntityManagerFactory();
+        EntityManager entityManager = entityManagerFactory.createEntityManager();
+        EntityTransaction entityTransaction = entityManager.getTransaction();
+        entityTransaction.begin();
+
+        User user = entityManager.find(User.class, "habib");
+
+        Wallet wallet = new Wallet();
+        wallet.setUser(user);
+        wallet.setBalance(1_000_000L);
+        entityManager.persist(wallet);
+
+        entityTransaction.commit();
+        entityManager.close();
+    }
+
+    @Test
+    void oneToOneJoinColumnFind() {
+        EntityManagerFactory entityManagerFactory = JpaUtil.getEntityManagerFactory();
+        EntityManager entityManager = entityManagerFactory.createEntityManager();
+        EntityTransaction entityTransaction = entityManager.getTransaction();
+        entityTransaction.begin();
+
+        User user = entityManager.find(User.class, "habib");
+        Assertions.assertNotNull(user.getCredential());
+        Assertions.assertNotNull(user.getWallet());
+
+        Assertions.assertEquals("habib@gmail.com", user.getCredential().getEmail());
+        Assertions.assertEquals("rahasia", user.getCredential().getPassword());
+        Assertions.assertEquals(1_000_000L, user.getWallet().getBalance());
+
+        entityTransaction.commit();
+        entityManager.close();
+    }
+
+    @Test
+    void oneToManyInsert() {
+        EntityManagerFactory entityManagerFactory = JpaUtil.getEntityManagerFactory();
+        EntityManager entityManager = entityManagerFactory.createEntityManager();
+        EntityTransaction entityTransaction = entityManager.getTransaction();
+        entityTransaction.begin();
+
+        Brand brand = new Brand();
+        brand.setId("samsung");
+        brand.setName("Samsung");
+        entityManager.persist(brand);
+
+        Product product1 = new Product();
+        product1.setId("p1");
+        product1.setName("Samsung Galaxy 1");
+        product1.setBrand(brand);
+        product1.setPrice(1_000_000L);
+        entityManager.persist(product1);
+
+        Product product2 = new Product();
+        product2.setId("p2");
+        product2.setName("Samsung Galaxy 2");
+        product2.setBrand(brand);
+        product2.setPrice(2_000_000L);
+        entityManager.persist(product2);
+
+        entityTransaction.commit();
+        entityManager.close();
+    }
+
+    @Test
+    void oneToManyFind() {
+        EntityManagerFactory entityManagerFactory = JpaUtil.getEntityManagerFactory();
+        EntityManager entityManager = entityManagerFactory.createEntityManager();
+        EntityTransaction entityTransaction = entityManager.getTransaction();
+        entityTransaction.begin();
+
+        Brand brand = entityManager.find(Brand.class, "samsung");
+        Assertions.assertNotNull(brand.getProducts());
+        Assertions.assertEquals(2, brand.getProducts().size());
+
+        brand.getProducts().forEach(product -> System.out.println(product.getName()));
+
+        entityTransaction.commit();
+        entityManager.close();
+    }
+}
